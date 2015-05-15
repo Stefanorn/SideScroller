5bd7dbd7c2ef6c5591ba808007e9a91ccbe4e8c1
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-05-15 14:04:47 +0000

    Animation
    Animiations á Player Carecter Jump, walk og idle

---
+++ Assets/PlayerSpriteRotater.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpriteRotater : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetAxis("Horizontal") == 1 ){
+			transform.rotation = Quaternion.Euler(0, 0,0);
+		}
+		if(Input.GetAxis("Horizontal") == -1 ) {
+			transform.rotation = Quaternion.Euler(0, 180,0);
+		}
+	}
+}
--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -8,28 +8,36 @@ public class playerMovement : MonoBehaviour {
 
 	
 	bool hasJumped = false;
-	float jumpDelay = 0.7f; //TODO harðkoðun laga
+	float jumpDelay = 0.5f; //TODO harðkoðun laga
 
 	Rigidbody2D rb;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponentInChildren<Animator>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
-		rb.AddForce( new Vector2( Input.GetAxis("Horizontal") * speed,0 ));
+		if(Input.GetAxis("Horizontal") != 0 ){ //TODO aðlaga animation speed að walking speed
+			anim.SetBool ("IsMoving", true);
+		}
+		else {
+			anim.SetBool ("IsMoving", false);
+		}
+		anim.SetBool("Jump", hasJumped);
+
+		rb.AddForce( new Vector2( Input.GetAxis("Horizontal") * speed,0 )); 
 
 		if( Input.GetAxis("Jump") > 0f  && !hasJumped)  {
 			rb.AddForce( new Vector2( 0, jumpForce ), ForceMode2D.Impulse );
-
 			hasJumped = true;
 		}
 		if(hasJumped){
 			jumpDelay -= Time.deltaTime;
 			if(jumpDelay <= 0){
-				jumpDelay = 0.7f;
+				jumpDelay = 0.5f;
 				hasJumped = false;
 			}
 		}
