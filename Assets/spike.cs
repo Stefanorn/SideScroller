af18c93a518f4ccef016f6ed3e3041a24e17b33d
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-05-17 01:20:33 +0000

    HÁLF KlARAÐ spike
    athuga corutine

---
+++ Assets/spike.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spike : MonoBehaviour {
+
+	public float startDelay = 0f;
+	public float repeatRate = 3f;
+	public float spikingSpeed = 2; //Mælt i sek
+	public float spikeDistance = 2f;
+
+	Vector3 originalPos;
+	// Use this for initialization
+	void Start () {
+		originalPos = this.transform.position;
+		InvokeRepeating("Darting",startDelay,repeatRate);
+
+		StartCoroutine("Darting",repeatRate);
+	}
+	
+	// Update is called once per frame
+	IEnumerator Darting () {
+		spikeDistance /= 10f;
+		Vector3 endPos = new Vector3( originalPos.x, 
+		                              originalPos.y + spikeDistance , 
+		                              originalPos.z );
+		while(true){
+
+			transform.position = Vector3.Lerp( originalPos, 
+			                                   endPos,
+			                                   spikingSpeed * Time.deltaTime );
+
+			yield return null;
+		}
+
+	}
+}
