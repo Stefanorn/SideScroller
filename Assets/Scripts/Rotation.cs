20eea10a7966032403daf4fe93e56e5063209a27
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-05-23 16:34:29 +0000

    Gerði Sawblade

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotationSpeed = 10f;
+
+	float rotationValue;
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		rotationValue += rotationSpeed*Time.deltaTime;
+		if(rotationValue >= 360){
+			rotationValue -= 360;
+		}
+		if(rotationValue <= -360){
+			rotationValue += 360;
+		}
+		Debug.Log( rotationValue );
+
+		transform.rotation = Quaternion.AngleAxis(rotationValue,Vector3.forward);
+	}
+}
---
+++ Assets/Scripts/sawbladePingPong.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sawbladePingPong : MonoBehaviour {
+	
+	public Transform targetLocation;
+	public Transform sawbladeGO;
+	public float timeFromAtoB = 2f;
+
+
+	float timer = 0f;
+	Vector3 originalLocation;
+	
+	// Use this for initialization
+	void Start () {
+		
+		originalLocation = sawbladeGO.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		timer += Time.deltaTime*timeFromAtoB;
+		if(timer > 1f){
+			timeFromAtoB *= -1f;
+		}
+		if(timer < -0.0f){
+			timeFromAtoB *= -1f;
+		}
+		sawbladeGO.position = Vector3.Lerp( originalLocation, targetLocation.position, timer );
+
+	}
+}
