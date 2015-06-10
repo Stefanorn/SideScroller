970984cf9073b61bd407e7b72f25afd06ab5b8ea
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-06-10 15:42:16 +0000

    Lazer og ailien platform og fínpússaði mapbuilder

--- Assets/Scripts/mapBuilder.cs
+++ Assets/Scripts/mapBuilder.cs
@@ -8,6 +8,7 @@ public class groundTypes{
 	public GameObject middleBlock;
 	public GameObject rightCorner;
 	public GameObject leftCorner;
+	public GameObject centerBlock;
 }
 
 
@@ -16,15 +17,25 @@ public class mapBuilder : MonoBehaviour {
 
 	public groundTypes[] ground;
 
-	[Range (0, 3)]
+	[Range (0, 4)]
 	public int groundPicker = 0;
 	int groundPickerUpdater = 0;
 	[Range (1,30)]
 	public  int lenght = 3;
+	[Range (0,30)]
+	public int leftWall = 0;
+	[Range (0,30)]
+	public int rightWall = 0;
 
 	int offset = 0;
 
 	void Update() {
+
+
+		transform.position = new Vector3( (int)transform.position.x,
+		                                  (int)transform.position.y,
+		                                  (int)transform.position.z );
+
 		if(groundPicker > ground.Length){ //Watches that you dont get a out of arry index error
 			groundPicker = 0;
 		}
@@ -45,6 +56,9 @@ public class mapBuilder : MonoBehaviour {
 					gameObject.GetComponent<BoxCollider2D>().usedByEffector = true;
 					gameObject.AddComponent<SurfaceEffector2D>().forceScale = 0.7f;
 					gameObject.GetComponent<SurfaceEffector2D>().speed = 0f;
+				}
+				else if(groundPicker == 4){ //SpacePlatform Parhaps need to optimize
+					gameObject.GetComponent<BoxCollider2D>().enabled = false;
 				}
 			}
 			Transform[] childs;
@@ -85,13 +99,16 @@ public class mapBuilder : MonoBehaviour {
 			DestroyImmediate(gameObject.GetComponent<PlatformEffector2D>());
 		}
 		gameObject.GetComponent<BoxCollider2D>().usedByEffector = false;
+		gameObject.GetComponent<BoxCollider2D>().enabled = true;
+
 		gameObject.tag = "Untagged";
+		
 	}
 	void InstaciateGroundType( GameObject groundTrans){ //Takes in the right GameObject and instanciates it to the right place
 
 		Vector3 platformPos = new Vector3(	transform.position.x + (float)offset, //Finds the right place
-		                                  transform.position.y,
-		                                  transform.position.z);
+		                                	transform.position.y,
+		                                	transform.position.z);
 
 		GameObject instaceOfGround = (GameObject)Instantiate( groundTrans, 
 		                                                      platformPos,
---
+++ Assets/shootLazer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootLazer : MonoBehaviour {
+
+	public GameObject lazer;
+	public float bulletLifeTime = 4f;
+	public float cooldownDelay = 2f;
+	public float refreshRate = 1f;
+	public float offset = 0.5f;
+	public float maxRange = 5f;
+	
+
+	float offsetSum = 0f;
+
+
+	void Start () {
+		Invoke("InstanciateLazer", refreshRate);
+	}
+	void InstanciateLazer(){
+
+		GameObject lazerInstance = (GameObject)Instantiate(	lazer, 
+		            										new Vector3((transform.position.x + offsetSum),
+		                            	 								transform.position.y,
+		                           	  	 								transform.position.z ),
+		           	 										Quaternion.identity);
+		lazerInstance.transform.SetParent(this.transform);
+		Destroy(lazerInstance, bulletLifeTime);
+
+		offsetSum += offset;
+		if(offsetSum > maxRange){
+			Invoke("InstanciateLazer", cooldownDelay );
+			offsetSum = 0f;
+		}
+		else
+			Invoke("InstanciateLazer", refreshRate);
+	}
+}
