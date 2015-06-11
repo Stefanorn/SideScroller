bfcbbd6db44ffb87f0a551cfa5a3b3b8dc6bda29
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-06-11 14:24:51 +0000

    Kláraði að gera WorldBuilder scriptið
    nú gerir það veggi fullkomlega

--- Assets/Scripts/mapBuilder.cs
+++ Assets/Scripts/mapBuilder.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [System.Serializable]
@@ -19,7 +19,7 @@ public class mapBuilder : MonoBehaviour {
 
 	[Range (0, 4)]
 	public int groundPicker = 0;
-	int groundPickerUpdater = 0;
+
 	[Range (1,30)]
 	public  int lenght = 3;
 	[Range (0,30)]
@@ -28,23 +28,22 @@ public class mapBuilder : MonoBehaviour {
 	public int rightWall = 0;
 
 	int offset = 0;
-	bool hasReachedEndOfGround = false;
+	int groundPickerUpdater = 0;
 
 	void Update() {
+        int blockSum = lenght + leftWall + rightWall;
 
-
-		transform.position = new Vector3( (int)transform.position.x,
+        transform.position = new Vector3( (int)transform.position.x,
 		                                  (int)transform.position.y,
 		                                  (int)transform.position.z );
 
 		if(groundPicker > ground.Length){ //Watches that you dont get a out of arry index error
 			groundPicker = 0;
 		}
-		if(gameObject.transform.childCount != (lenght + leftWall + rightWall) || groundPicker != groundPickerUpdater){ //Destroys all GameObject if the Legngth is changed
+		if(gameObject.transform.childCount != blockSum || groundPicker != groundPickerUpdater){ //Destroys all GameObject if the Legngth is changed
 			if(groundPicker != groundPickerUpdater ){ //Adds and removes compoments and changes setting foreach platform
 				DefaultState();                       //Reset everything to default
-
-				if (groundPicker == 1){ //One Sided platform
+                if (groundPicker == 1){ //One Sided platform
 					gameObject.GetComponent<BoxCollider2D>().usedByEffector = true;
 					gameObject.AddComponent<PlatformEffector2D>();
 				}
@@ -73,31 +72,38 @@ public class mapBuilder : MonoBehaviour {
 			col.size = new Vector2( (float)lenght , col.size.y );
 		}
 		offset = 0; //offsets the tiles 1 unit to the left and also helps with choosing of the tile image
-		while(gameObject.transform.childCount != (lenght + leftWall + rightWall)  || groundPicker != groundPickerUpdater){ //Innstanciates the game objects if the lenght is changed
-				if( (lenght + leftWall + rightWall) > 95){
-					Debug.LogError(	"infinityloop in MainWhileLoop  gameObject.transform.childCount "  + 
-									gameObject.transform.childCount + 
-									" != "+ 
-									" (lenght + leftWall + rightWall) "+ 
-									(lenght + leftWall + rightWall)  + 
-									" groundPicker != "
-									+ groundPickerUpdater);
-				break;
-			}
+		while(gameObject.transform.childCount != blockSum || groundPicker != groundPickerUpdater){ //Innstanciates the game objects if the lenght is changed
+            
 			groundPickerUpdater = groundPicker;
-			if(lenght == 1){ 
+			if(lenght == 1 && rightWall == 0 && leftWall == 0){   //Calls for instaciate when this is only a single block
 				InstaciateGroundType( ground[groundPicker].singleBlock );
 			}
-			else {
-				if(offset == 0){
-					InstaciateGroundType( ground[groundPicker].leftCorner );
-				}
-				else if(offset == (lenght-1)){
-					InstaciateGroundType( ground[groundPicker].rightCorner );
-				}
-				else {
-					InstaciateGroundType( ground[groundPicker].middleBlock);
-				}
+			else { // calls for a instaciate when the are speccial cases
+                if (offset == 0 && leftWall == 0) 
+                {
+                    InstaciateGroundType(ground[groundPicker].leftCorner);
+                }
+                else if (offset == (lenght - 1) && rightWall == 0)
+                {
+                    InstaciateGroundType(ground[groundPicker].rightCorner);
+                }
+                else if (rightWall >= 1 && offset == (lenght - 1)) 
+                {
+
+                    InstaciateGroundType(ground[groundPicker].centerBlock);
+                }
+                else if (leftWall >= 1 && offset == 0)
+                {
+                    InstaciateGroundType(ground[groundPicker].centerBlock);
+                }
+                else if (offset > lenght) { 
+                    InstaciateGroundType(ground[groundPicker].centerBlock);
+                }
+                else
+                {
+                    InstaciateGroundType(ground[groundPicker].middleBlock);
+                }
+
 			}
 
 		}
@@ -117,37 +123,38 @@ public class mapBuilder : MonoBehaviour {
 	}
 	void InstaciateGroundType( GameObject groundTrans){ //Takes in the right GameObject and instanciates it to the right place
 		int wallOffset = 0;
+        if (offset < lenght)
+        {
+            Vector3 platformPos = new Vector3(transform.position.x + (float)offset, //Finds the right place
+                                                transform.position.y,
+                                                transform.position.z);
 
-		Vector3 platformPos = new Vector3(	transform.position.x + (float)offset, //Finds the right place
-		                                	transform.position.y,
-		                                	transform.position.z);
-
-		GameObject instaceOfGround = (GameObject)Instantiate( groundTrans, 
-		                                                      platformPos,
-		                                                      Quaternion.identity );
-		while(offset == 0  && leftWall != wallOffset ){
-			Vector3 wallPos = new Vector3(	transform.position.x + (float)offset, //Finds the right place
-			                                  transform.position.y + (float)wallOffset,
-			                                  transform.position.z);
+            GameObject instaceOfGround = (GameObject)Instantiate(groundTrans,
+                                                                  platformPos,
+                                                                  Quaternion.identity);
+            instaceOfGround.transform.SetParent(gameObject.transform);
+        }
+        while (offset == 0  && leftWall != wallOffset ){
+			Vector3 wallPos = new Vector3(	transform.position.x + offset, //Finds the right place
+			                                transform.position.y + wallOffset +1,
+			                                transform.position.z);
 			GameObject instaceOfWall = (GameObject)Instantiate(   groundTrans, 
 			                                                      wallPos,
 			                                                      Quaternion.identity );
 			instaceOfWall.transform.SetParent(gameObject.transform); 
 			wallOffset++;
 		}
-		if( offset == lenght) {
-			Debug.Log("gagaga");
-			Vector3 wallPos = new Vector3(	transform.position.x + (float)offset, //Finds the right place
-			                             	transform.position.y,
-			                              	transform.position.z);
-			GameObject instaceOfWall = (GameObject)Instantiate( groundTrans, 
-			                                                  	wallPos,
-			                                                   	Quaternion.identity );
-			instaceOfWall.transform.SetParent(gameObject.transform); 
-			wallOffset++;
-		}
-		//Debug.Log((lenght + rightWall) + " " + (offset + 1) );
-		instaceOfGround.transform.SetParent(gameObject.transform); 
-		offset++;
+        if(offset > lenght)
+        {
+           Vector3 wallPos = new Vector3(	transform.position.x + lenght-1, //Finds the right place
+			                                transform.position.y + (offset - lenght ),
+			                                transform.position.z);
+			GameObject instaceOfWall = (GameObject)Instantiate(   groundTrans, 
+			                                                      wallPos,
+			                                                      Quaternion.identity );
+            instaceOfWall.transform.SetParent(gameObject.transform);
+        }
+        //Debug.Log((lenght + rightWall) + " " + (offset + 1) );
+        offset++;
 	}
 }
